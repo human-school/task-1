7d6f2666c2937b12c9ce241d6b47a067a7211be0
human <95416949+human-school@users.noreply.github.com>
2022-11-29 09:20:34 +1100

    start

---
+++ Templates/BONES.dwt.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+	<link rel='icon' href='favicon.ico' type='image/x-icon'/ >
+<meta charset="utf-8">
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>NEVER STOP</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+</head>
+
+<body bgcolor = "black">
+<table width="100%" border="1">
+  <tbody>
+    <tr>
+      <td width="8%" height="82" align="left" valign="top" style="color: #FFFFFF"><a href="../index.php"><img src="../images/a-hate-crime.png" width="90" height="90" alt=""/></a></td>
+      <td width="92%" align="left" style="color: #FFFFFF">AAAAAAAAAAAAAA</td>
+    </tr>
+    <tr>
+      <td height="3705" style="color: #FFFFFF" align="left" valign="top">more pages</td>
+      <td align="left" style="color: #FFFFFF" valign="top"><!-- TemplateBeginEditable name="main" -->AAAAAA+content<!-- TemplateEndEditable --></td>
+    </tr>
+  </tbody>
+</table>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html><!-- InstanceBegin template="/Templates/BONES.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<head>
+	<link rel='icon' href='favicon.ico' type='image/x-icon'/ >
+<meta charset="utf-8">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>NEVER STOP</title>
+<!-- InstanceEndEditable -->
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+</head>
+
+<body bgcolor = "black">
+<table width="100%" border="1">
+  <tbody>
+    <tr>
+      <td width="8%" height="82" align="left" valign="top" style="color: #FFFFFF"><a href="index.php"><img src="images/a-hate-crime.png" width="90" height="90" alt=""/></a></td>
+      <td width="92%" align="left" style="color: #FFFFFF">AAAAAAAAAAAAAA</td>
+    </tr>
+    <tr>
+      <td height="3705" style="color: #FFFFFF" align="left" valign="top">more pages</td>
+      <td align="left" style="color: #FFFFFF" valign="top"><!-- InstanceBeginEditable name="main" -->AAAAAA+content<!-- InstanceEndEditable --></td>
+    </tr>
+  </tbody>
+</table>
+</body>
+<!-- InstanceEnd --></html>
