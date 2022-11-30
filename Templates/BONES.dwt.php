07654449e5aab73d37086df2bee892869b5b45d3
human <95416949+human-school@users.noreply.github.com>
2022-12-01 10:12:28 +1100

    AAAAAAA

--- Templates/BONES.dwt.php
+++ Templates/BONES.dwt.php
@@ -11,15 +11,16 @@
 </head>
 
 <body bgcolor = "black">
-<table width="100%" border="1">
+<table width="100%" border="0">
   <tbody>
     <tr>
-      <td width="8%" height="82" align="left" valign="top" style="color: #FFFFFF"><a href="../index.php"><img src="../images/a-hate-crime.png" width="90" height="90" alt=""/></a></td>
-      <td width="92%" align="left" style="color: #FFFFFF">AAAAAAAAAAAAAA</td>
+      <td width="8%" height="94" align="left" valign="top" style="color: #FFFFFF"><a href="../index.php"><img src="../images/a-hate-crime.png" alt="home" width="90" height="90" title="home"/></a></td>
+      <td width="92%" align="left" valign="top" style="color: #FFFFFF"><img src="../images/github.png" width="1114" height="85" alt=""/></td>
     </tr>
     <tr>
-      <td height="3705" style="color: #FFFFFF" align="left" valign="top">more pages</td>
-      <td align="left" style="color: #FFFFFF" valign="top"><!-- TemplateBeginEditable name="main" -->AAAAAA+content<!-- TemplateEndEditable --></td>
+      <td width="100%" height="3705" style="color: #FFFFFF" align="left" valign="top"><p><a href="../index.php"><img src="../images/home.png" alt="home" width="100" height="60" title="home"/></a></p>
+      <p><a href="../AAAAAAAAAA.php"><img src="../images/info.png" alt="AAAAAAAA" width="100" height="60" title="AAAAAAAA"/></a></p></td>
+      <td width="100%" align="left" style="color: #FFFFFF" valign="top"><!-- TemplateBeginEditable name="main" -->AAAAAA+content<!-- TemplateEndEditable --></td>
     </tr>
   </tbody>
 </table>
--- index.php
+++ index.php
@@ -11,15 +11,20 @@
 </head>
 
 <body bgcolor = "black">
-<table width="100%" border="1">
+<table width="100%" border="0">
   <tbody>
     <tr>
-      <td width="8%" height="82" align="left" valign="top" style="color: #FFFFFF"><a href="index.php"><img src="images/a-hate-crime.png" width="90" height="90" alt=""/></a></td>
-      <td width="92%" align="left" style="color: #FFFFFF">AAAAAAAAAAAAAA</td>
+      <td width="8%" height="94" align="left" valign="top" style="color: #FFFFFF"><a href="index.php"><img src="images/a-hate-crime.png" alt="home" width="90" height="90" title="home"/></a></td>
+      <td width="92%" align="left" valign="top" style="color: #FFFFFF"><img src="images/github.png" width="1114" height="85" alt=""/></td>
     </tr>
     <tr>
-      <td height="3705" style="color: #FFFFFF" align="left" valign="top">more pages</td>
-      <td align="left" style="color: #FFFFFF" valign="top"><!-- InstanceBeginEditable name="main" -->AAAAAA+content<!-- InstanceEndEditable --></td>
+      <td width="100%" height="3705" style="color: #FFFFFF" align="left" valign="top"><p><a href="index.php"><img src="images/home.png" alt="home" width="100" height="60" title="home"/></a></p>
+      <p><a href="AAAAAAAAAA.php"><img src="images/info.png" alt="AAAAAAAA" width="100" height="60" title="AAAAAAAA"/></a></p></td>
+      <td width="100%" align="left" style="color: #FFFFFF" valign="top"><!-- InstanceBeginEditable name="main" -->
+        <p>where dreams to to die</p>
+		  <p>impero kills all productivity</p>
+		  <p>in fact all site blocker and limiters KILL productivity</p>
+      <!-- InstanceEndEditable --></td>
     </tr>
   </tbody>
 </table>
